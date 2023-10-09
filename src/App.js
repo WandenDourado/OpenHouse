4dce41f7c6a6ee4ecf39e3fdb14208e474f297b0
Guilherme Dourado <guilhermedouradosantos@gmail.com>
2023-10-09 16:06:06 -0300

    develop

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload teste.
+          Edit <code>src/App.js</code> and save to reload house.
         </p>
         <a
           className="App-link"
