7c41b14deb4c427435fb126baca9c0de81d4c287
WandenDourado <WandenDourado@users.noreply.github.com>
2023-10-10 03:24:05 +0000

    Atualizar build

---
+++ precache-manifest.b5b49badc6596ee266acebb9b028c3c9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7dc1c0346674a53e23e221b9349eb71",
+    "url": "/open-house/index.html"
+  },
+  {
+    "revision": "13747ffe03e431dc120c",
+    "url": "/open-house/static/css/2.34507a92.chunk.css"
+  },
+  {
+    "revision": "85554e147df808833186",
+    "url": "/open-house/static/css/main.a8edbda8.chunk.css"
+  },
+  {
+    "revision": "13747ffe03e431dc120c",
+    "url": "/open-house/static/js/2.37eace97.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/open-house/static/js/2.37eace97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6676cf8d64c57f97b76",
+    "url": "/open-house/static/js/3.92e92791.chunk.js"
+  },
+  {
+    "revision": "85554e147df808833186",
+    "url": "/open-house/static/js/main.f19755e5.chunk.js"
+  },
+  {
+    "revision": "96c6235034ab689ade3a",
+    "url": "/open-house/static/js/runtime-main.6746f1d4.js"
+  }
+]);
