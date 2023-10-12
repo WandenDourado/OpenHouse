9138e96aeabd5bfa473c2a2721d310827b9434cd
Guilherme Dourado <guilhermedouradosantos@gmail.com>
2023-10-12 19:31:02 -0300

    toques finais

--- src/App.js
+++ src/App.js
@@ -2,42 +2,68 @@ import React from 'react'
 import Produto from "./componentes/produto";
 import './App.css';
 import home from './img/banner.png';
-
+import kit_um from './img/kitum.jpg';
+import kit_dois from './img/kitdois.jpg';
+import kit_tres from './img/kittres.jpg';
+import kit_quatro from './img/kitquatro.jpg';
+import QR_50 from './img/50.png'
+import QR_75 from './img/75.png'
+import QR_100 from './img/100.png'
+import QR_CORACAO from './img/coracao.png'
 
 function App() {
   
   const lista = [
     {
         id: "produtoUm",
-        valor: "R$25,00",
+        valor: "R$50,00",
+        imagem: kit_um,
+        code: "00020126560014BR.GOV.BCB.PIX0111321403228350219Gui Open House R$50520400005303986540550.005802BR5924GUILHERME DOURADO SANTOS6009SAO PAULO622605225iCOhoz3d5CzClOeD4QD7l630442FB",
+        qr: QR_50,
         itens: [
-            "Canecas personalizadas",
-            "Jogos de copos",
-            "Porta-retratos",
-            "Panos de prato decorativos",
-            "Vasos de flores ou plantas pequenas"
+            "Jogo de xícaras",
+            "Jogo de copos",
+            "Pano de pratos",
+            "Jogo de talheres",
+            "Conjunto de potes"
         ]
     },
     {
         id: "produtoDois",
-        valor: "R$50,00",
+        valor: "R$75,00",
+        imagem: kit_dois,
+        code: "00020126560014BR.GOV.BCB.PIX0111321403228350219Gui Open House R$75520400005303986540575.005802BR5924GUILHERME DOURADO SANTOS6009SAO PAULO622605221wW9T0K3qx2RHHonc7BVfT63047B10",
+        qr: QR_75,
         itens: [
-            "Conjunto de lençóis",
-            "Conjunto de panelas",
-            "Cafeteira ou chaleira elétrica",
-            "Jogo de talheres",
-            "Conjunto de potes de armazenamento"
+            "Jogos de pratos",
+            "Jogos de faca",
+            "Chaleira",
+            "Jogo de banheiro",
+            "Lixeira"
         ]
     },
     {
         id: "produtoTres",
-        valor: "R$75,00",
+        valor: "R$100,00",
+        imagem: kit_tres,
+        code: "00020126570014BR.GOV.BCB.PIX0111321403228350220Gui Open House R$1005204000053039865406100.005802BR5924GUILHERME DOURADO SANTOS6009SAO PAULO622605222Cc60ZXgpRawEjqlSJBW846304B2CF",
+        qr: QR_100,
         itens: [
             "Liquidificador",
-            "Grill ou churrasqueira elétrica",
-            "Jogo de pratos",
-            "Kit de organização para armários",
-            "Balança de cozinha"
+            "Air fryer",
+            "Jogo de panelas",
+            "Jogo de lençol",
+            "Kit de toalhas"
+        ]
+    },
+    {
+        id: "produtoQuatro",
+        imagem: kit_quatro,
+        code: "00020126540014BR.GOV.BCB.PIX0111321403228350217Gui Open House <35204000053039865802BR5924GUILHERME DOURADO SANTOS6009SAO PAULO622605224ENQiLLDAQCObvGrKXkZFn63049433",
+        qr: QR_CORACAO,
+        valor: "O que seu coração mandar",
+        itens: [
+            "Aquele produto muito especial, que irá me ajudar muito nessa nova fase da minha vida!"
         ]
     }
 ];
@@ -53,8 +79,8 @@ function App() {
     <img src={home} alt="home"/>
       <p>Olá amigos, bem vindos ao meu Chá de casa nova!</p>
     </div>
-    <div class="row">
-      <p>Segue produtos</p>
+    <div class="row justify">
+      <p>Segue a lista de produtos para aqueles que puderem ajudar-me nesta nova fase da minha vida. Serei profundamente grato por qualquer quantia que possam contribuir.</p>
     </div>
   <div class="row">
     <div class="col">
@@ -66,6 +92,12 @@ function App() {
     <div class="col">
       <Produto produto={lista[2]}/>
     </div>    
+  </div>
+
+  <div class="row">
+    <div class="col">
+    <Produto produto={lista[3]}/>
+    </div>
   </div>
 
 </div>
--- src/componentes/produto/produto.jsx
+++ src/componentes/produto/produto.jsx
@@ -1,7 +1,5 @@
 import "./produto.css";
 import React from "react";
-import pratos from '../../img/pratos.jpg';
-import QR_25 from '../../img/25.png';
 import bootstrap from 'bootstrap/dist/js/bootstrap.bundle';
 
 
@@ -11,12 +9,14 @@ function ProdutoJSX({ produto }) {
 
 function template(produto) {
 
+  const produto_ = produto;
+
 
   function handleClick(){
-    var exampleTriggerEl = document.getElementById('example')
+    var exampleTriggerEl = document.getElementById(`popover${produto.id}`)
     var popover = bootstrap.Popover.getOrCreateInstance(exampleTriggerEl) // Returns a Bootstrap popover instance
     popover.toggle();
-    const texto = "00020126560014BR.GOV.BCB.PIX0111321403228350219Gui Open House R$25520400005303986540525.005802BR5924GUILHERME DOURADO SANTOS6009SAO PAULO622605222TOf4Z5nSlEKogiK6gxlGL6304EFC2";
+    const texto = produto_.code;
     if (navigator.clipboard) {
       navigator.clipboard.writeText(texto)
         .then(function() {
@@ -34,7 +34,7 @@ function template(produto) {
     <div class="fundo">
       <div class="row">
         <div class="col">
-        <img class="redondo" src={pratos} alt="pratos" />
+        <img class="redondo" style={{ width: '350px', height: '280px' }} src={produto.imagem} alt="kit" />
         </div>
         <div class="col left">
           <div class="row">
@@ -71,11 +71,11 @@ function template(produto) {
           <div class="row">
             <div class="col">
               <p>Valor: {produto.valor}</p>
-            <img src={QR_25} alt="25" style={{ width: '250px', height: '250px' }} />
-              <button id="example" type="button" onClick={handleClick} class="btn btn-lg btn-success" data-bs-toggle="example" title="Pix copia e cola" data-bs-content="Código pix copiado para sua área de trasnferência, agora só ir para o seu app e colar.">Pix copia e cola</button>
+            <img src={produto.qr} alt="25" style={{ width: '250px', height: '250px' }} />
+              <button id={`popover${produto.id}`} type="button" onClick={handleClick} class="btn btn-lg btn-success" data-bs-toggle={`popover${produto.id}`} title="Pix copia e cola" data-bs-placement="top" data-bs-content="Código pix copiado para sua área de trasnferência, agora só ir para o seu app e colar.">Pix copia e cola</button>
             </div>
             <div class="col">
-              <p>Para realizar o pagamento scanei o QR Code utilizando o app do seu banco ou utilize o botão "Pix copia e cola" para copiar o código e então cole-o no app do seu banco!</p>
+              <p>Para realizar o pagamento escaneie o QR Code utilizando o app do seu banco ou utilize o botão "Pix copia e cola" para copiar o código e então cole-o no na <b>área Pix copia e cola</b> no app do seu banco!</p>
             </div>
           </div>
           <div class="row">
