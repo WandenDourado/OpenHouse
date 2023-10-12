58aeb7b9d2652e4fb66b96b398558af769e9bb8a
WandenDourado <WandenDourado@users.noreply.github.com>
2023-10-12 01:49:09 +0000

    Atualizar build

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/open-house/precache-manifest.564a1e277a61120f4a5a9d0c0ee447e5.js"
+  "/open-house/precache-manifest.b069b91b657302ed46bf8d585002cace.js"
 );
 
 self.addEventListener('message', (event) => {
